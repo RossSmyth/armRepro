a6ea371325b1d7be0668f2e0c213e4698563b8a9
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2025-05-06 12:45:33 -0400

    Using the native package set

--- flake.nix
+++ flake.nix
@@ -16,10 +16,12 @@
       pkgsCross = import nixpkgs {
         localSystem = pkgs.stdenv.buildPlatform;
         crossSystem = {
+          # The wrapper uses this as the target prefix, so this must be "arm-none-eabi"
           config = "arm-none-eabi";
           # gcc-arm-embedded has bundled libcs, so we will pass that in ourselves.
           libc = null;
           gcc = {
+            # Need to set all these manually because of funny reasons
             thumb = true;
             cpu = "cortex-m0";
             float-abi = "softfp";
@@ -28,17 +30,25 @@
         };
       };
 
-      stdenv = pkgsCross.overrideCC pkgsCross.stdenv (pkgsCross.wrapCCWith {
-        nativeTools = false;
-        coreutils = pkgs.coreutils;
+      stdenv = pkgs.overrideCC pkgsCross.stdenv (pkgs.wrapCCWith {
         name = "embedded-wrapped";
+        # Not strictly needed.
         noLibc = true;
+        
+        stdenvNoCC = pkgsCross.stdenvNoCC;
+
+        # GCC is at this pkgs/bin/gcc
         cc = pkgs.gcc-arm-embedded;
+
+        # Unsure if this is needed, but probably good to have.
         bintools = pkgs.wrapBintoolsWith {
-          nativeTools = false;
-          coreutils = pkgs.coreutils;
+          stdenvNoCC = pkgsCross.stdenvNoCC;
           noLibc = true;
+
+          # Again, we will try. May need to pass the specs with some extra flags here too?
           libc = null;
+
+          # at pkg/bin
           bintools = pkgs.gcc-arm-embedded;
           isGNU = true;
         };
@@ -53,6 +63,7 @@
     {
       packages.${system} = {
         default = firmware;
+        # For debugging
         cc = stdenv.cc;
       };
     };
